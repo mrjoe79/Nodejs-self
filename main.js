05c1a7f1e0ae42263fce794bf779bf14ba0791b8
mrjoe79 <whgodyd79@naver.com>
2023-04-10 23:26:05 +0900

    update, delete 처리

--- main.js
+++ main.js
@@ -54,7 +54,13 @@ var app = http.createServer (function(request, response){     //var http = requi
           var title = queryData.id;           //HTML만 추출
           var list = templateList(filelist);
           var template = templateHTML(title, list, `<h2>${title}</h2> ${description}`,
-          `<a href ="/create">create</a> <a href="/update?id=${title}">update</a>`
+          ` <a href ="/create">create</a> 
+            <a href="/update?id=${title}">update</a>
+            <form action="delete_process" method="post">
+              <input type ="hidden" name="id" value="${title}">
+              <input type ="submit" value="delete">
+            </form>
+            `
           );  
           response.writeHead(200);
           response.end(template);
@@ -117,8 +123,37 @@ var app = http.createServer (function(request, response){     //var http = requi
           response.end(template);
       }); 
      });
-    }
-      else {
+    } else if (pathname === '/update_process') {
+      var body = '';
+      request.on('data', function(data){
+          body = body + data;
+      });
+      request.on('end', function(){
+          var post = qs.parse(body);
+          var id = post.id;
+          var title = post.title;
+          var description = post.description;
+          fs.rename(`data/${id}`, `data/${title}`, function(error){
+            fs.writeFile(`data/${title}`, description, 'utf8', function(err){
+              response.writeHead(302, {location: `/?id=${title}`});
+              response.end();
+            })
+          });
+      });  
+    } else if (pathname === '/delete_process') {
+      var body = '';
+      request.on('data', function(data){
+          body = body + data;
+      });
+      request.on('end', function(){
+          var post = qs.parse(body);
+          var id = post.id;
+          fs.unlink(`data/${id}`, function(error){
+            response.writeHead(302, {location: `/`});
+            response.end();
+          })  
+      });  
+    } else {
       response.writeHead(404);
       response.end('Not found'); 
   } 
