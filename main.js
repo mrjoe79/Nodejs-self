9a8b2ce68644224e39ade4fd7f407b86965c7987
mrjoe79 <whgodyd79@naver.com>
2023-04-17 23:56:26 +0900

    done

--- main.js
+++ main.js
@@ -2,34 +2,11 @@ var http = require('http');             //http모듈을 포함하려면, require
 var fs = require('fs')                  //filesystem 모듈을 포함하려면, require 메소드를 사용한다.
 var url = require('url');
 var qs = require('querystring');
+var template = require('./lib/template.js');
+var path = require('path');
+var sanitizeHtml = require('sanitize-html');
 
-function templateHTML(title, list, body, control){
-  return `
-  <!doctype html>
-  <html>
-  <head>
-    <title>WEB1 - ${title}</title>
-    <meta charset="utf-8">
-  </head>
-  <body>
-    <h1><a href="/">WEB2</a></h1>
-    ${list}
-    ${control}    
-    ${body}
-  </body>
-  </html>
-  `; 
-}
-function templateList(filelist){
-  var list =  '<ul>';
-  var i = 0;
-    while(i<filelist.length){
-      list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
-      i = i+1;
-    }
-    list = list+'</ul>';
-    return list;
-}
+
 
 var app = http.createServer (function(request, response){     //var http = require('http'); 
     var _url = request.url;             //console.log(_url)을 찍어보면, id?HTML라는 값이 _url 변수에 담겨짐
@@ -40,38 +17,43 @@ var app = http.createServer (function(request, response){     //var http = requi
         fs.readdir('./data', function(error, filelist){
           var title = 'Welcome';
           var description = 'Hello, Node.js';
-          var list = templateList(filelist);              //요거??
-          var template = templateHTML(title, list, 
+
+          var list = template.List(filelist);              //요거??
+          var html = template.HTML(title, list, 
             `<h2>${title}</h2> ${description}`, 
             `<a href ="/create">create</a>`
             );   //요거??
           response.writeHead(200);
-          response.end(template);                  
+          response.end(html);                 
+
         });
     } else {
-      fs.readdir('./data', function(error, filelist){       
-        fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
+      fs.readdir('./data', function(error, filelist){    
+        var filteredId = path.parse(queryData.id).base;
+        fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
           var title = queryData.id;           //HTML만 추출
-          var list = templateList(filelist);
-          var template = templateHTML(title, list, `<h2>${title}</h2> ${description}`,
+          var sanitizedTitle = sanitizeHtml(title);
+          var sanitizedDescription = sanitizeHtml(description);
+          var list = template.List(filelist);
+          var html = template.HTML(sanitizedTitle, list, `<h2>${sanitizedTitle}</h2> ${sanitizedDescription}`,
           ` <a href ="/create">create</a> 
-            <a href="/update?id=${title}">update</a>
+            <a href="/update?id=${sanitizedTitle}">update</a>
             <form action="delete_process" method="post">
-              <input type ="hidden" name="id" value="${title}">
+              <input type ="hidden" name="id" value="${sanitizedTitle}">
               <input type ="submit" value="delete">
             </form>
             `
           );  
           response.writeHead(200);
-          response.end(template);
+          response.end(html);
       }); 
      });
     }      
   } else if(pathname === '/create') {
     fs.readdir('./data', function(error, filelist){
       var title = 'WEB - create';      
-      var list = templateList(filelist);              //요거??
-      var template = templateHTML(title, list, `
+      var list = template.List(filelist);              //요거??
+      var html = template.HTML(title, list, `
         <form action="/create_process" method="post">
           <p><input type="text" name="title"></p>
           <p>
@@ -83,7 +65,7 @@ var app = http.createServer (function(request, response){     //var http = requi
       </form>
     `, '');   //요거??
       response.writeHead(200);
-      response.end(template);               
+      response.end(html);               
   });
  } else if(pathname === '/create_process'){
       var body = '';
@@ -100,11 +82,12 @@ var app = http.createServer (function(request, response){     //var http = requi
           })
       });      
     } else if(pathname === `/update`) {
-      fs.readdir('./data', function(error, filelist){       
-        fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
+      fs.readdir('./data', function(error, filelist){     
+        var filteredId = path.parse(queryData.id).base;  
+        fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
           var title = queryData.id;           //HTML만 추출
-          var list = templateList(filelist);
-          var template = templateHTML(title, list,                   
+          var list = template.List(filelist);
+          var html = template.HTML(title, list,                   
           `
           <form action="/update_process" method="post">
           <input type = "hidden" name = "id" value = ${title}>
@@ -120,7 +103,7 @@ var app = http.createServer (function(request, response){     //var http = requi
           `<a href ="/create">create</a> <a href="/update?id=${title}">update</a>`
           );  
           response.writeHead(200);
-          response.end(template);
+          response.end(html);
       }); 
      });
     } else if (pathname === '/update_process') {
@@ -148,7 +131,8 @@ var app = http.createServer (function(request, response){     //var http = requi
       request.on('end', function(){
           var post = qs.parse(body);
           var id = post.id;
-          fs.unlink(`data/${id}`, function(error){
+          var filteredId = path.parse(id).base;
+          fs.unlink(`data/${filteredId}`, function(error){
             response.writeHead(302, {location: `/`});
             response.end();
           })  
